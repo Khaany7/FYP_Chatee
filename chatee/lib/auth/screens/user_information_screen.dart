8d67e53ffa0f9438fa4b1ce2107389e3c764587e
abd <abdurrehmankhan4946@.com>
2024-11-30 16:52:27 +0500

    user_information done

--- chatee/lib/auth/screens/user_information_screen.dart
+++ chatee/lib/auth/screens/user_information_screen.dart
@@ -1,11 +1,34 @@
+import 'dart:io';
+import 'package:chatee/utils/utils.dart';
 import 'package:flutter/material.dart';
 
-class UserInformationScreen extends StatelessWidget {
+class UserInformationScreen extends StatefulWidget {
   static const String routeName = '/user-information';
   const UserInformationScreen({super.key});
 
   @override
+  State<UserInformationScreen> createState() => _UserInformationScreenState();
+}
+
+class _UserInformationScreenState extends State<UserInformationScreen> {
+  final TextEditingController nameController = TextEditingController();
+  File? image;
+  @override
+  void dispose() {
+    nameController.dispose();
+    super.dispose();
+  }
+
+  void selectImage() async {
+    image = await pickImageFromGallery(context);
+    setState(() {
+      image = image;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         child: Center(
@@ -13,15 +36,45 @@ class UserInformationScreen extends StatelessWidget {
             children: [
               Stack(
                 children: [
-                  const CircleAvatar(
-                    backgroundImage: NetworkImage(
-                      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+                  image == null
+                      ? const CircleAvatar(
+                          backgroundImage: NetworkImage(
+                            'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+                          ),
+                          radius: 64,
+                        )
+                      : CircleAvatar(
+                          backgroundImage: FileImage(image!),
+                          radius: 64,
+                        ),
+                  Positioned(
+                    bottom: -10,
+                    left: 80,
+                    child: IconButton(
+                      onPressed: selectImage,
+                      icon: const Icon(
+                        Icons.add_a_photo,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    width: size.width * 0.85,
+                    padding: const EdgeInsets.all(20),
+                    child: TextField(
+                      controller: nameController,
+                      decoration: const InputDecoration(
+                        hintText: 'Full Name',
+                      ),
                     ),
                   ),
                   IconButton(
                     onPressed: () {},
                     icon: const Icon(
-                      Icons.add_a_photo,
+                      Icons.done,
                     ),
                   )
                 ],
